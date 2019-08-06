9159668c89633ff471f667e38c5005af82ec3dae
Satoshi Iwaki <satoshi.iwaki@gmail.com>
2019-08-06 23:45:42 +0900

    Update mermaid editor

--- dev-tools/diagram/libs/codemirror/mode/mermaid.js
+++ dev-tools/diagram/libs/codemirror/mode/mermaid.js
@@ -2,25 +2,130 @@ CodeMirror.defineSimpleMode("mermaid", {
   // The start state contains the rules that are intially used
   start: [
     {
-      regex: /graph|sequenceDiagram|gantt|classDiagram/,
-      token: "keyword"
+      regex: /graph/,
+      token: "keyword",
+      push: "graph",
+      indent: true
     },
     {
-      regex: /participant|loop|end|dateFormat|title|excludes|weekdays|section|done|active/,
-      token: "keyword"
+      regex: /gantt/,
+      token: "keyword",
+      push: "gantt",
+      indent: true
     },
+    {
+      regex: /sequenceDiagram/,
+      token: "keyword",
+      push: "sequenceDiagram",
+      indent: true
+    },
+    {
+      regex: /classDiagram/,
+      token: "keyword",
+      push: "classDiagram",
+      indent: true
+    },
+  ],
+  graph: [
     {
       regex: /TB|BT|RL|LR|TD/,
       token: "keyword"
     },
     {
+      regex: /-->|-.->|==>/,
+      token: "keyword"
+    },
+
+  ],
+  gantt: [
+    {
+      regex: /participant|note|dateFormat|title|excludes|weekdays|section|done|active/,
+      token: "keyword"
+    },
+
+  ],
+  // Sequence Diagram
+  sequenceDiagram: [
+    {
       regex: /-->>[-+]?|->>[-+]?|-->[-+]?|->[-+]?|--x[-+]?|-x[-+]?/,
       token: "keyword"
     },
     {
+      regex: /alt/,
+      token: "keyword",
+      indent: true,
+      push: "sequenceDiagramAlt"
+    },
+    {
+      regex: /opt/,
+      token: "keyword",
+      indent: true,
+      push: "sequenceDiagramOpt"
+    },
+    {
+      regex: /loop/,
+      token: "keyword",
+      indent: true,
+      push: "sequenceDiagramLoop"
+    },
+    {
+      regex: /note|Note/,
+      token: "keyword",
+      push: "sequenceDiagramNote"
+    },
+  ],
+  sequenceDiagramAlt: [
+    {
+      regex: /-->>[-+]?|->>[-+]?|-->[-+]?|->[-+]?|--x[-+]?|-x[-+]?/,
+      token: "keyword"
+    },
+    {
+      regex: /else/,
+      token: "keyword",
+    },
+    {
+      regex: /end/,
+      token: "keyword",
+      // dedent: true,
+      dedentIfLineStart: true,
+      pop: true
+    },
+  ],
+  sequenceDiagramOpt: [
+    {
+      regex: /-->>[-+]?|->>[-+]?|-->[-+]?|->[-+]?|--x[-+]?|-x[-+]?/,
+      token: "keyword"
+    },
+    {
+      regex: /end/,
+      token: "keyword",
+      dedent: true,
+      pop: true
+    },
+  ],
+  sequenceDiagramLoop: [
+    {
+      regex: /-->>[-+]?|->>[-+]?|-->[-+]?|->[-+]?|--x[-+]?|-x[-+]?/,
+      token: "keyword"
+    },
+    {
+      regex: /end/,
+      token: "keyword",
+      dedent: true,
+      pop: true
+    },
+  ],
+  sequenceDiagramNote: [
+    {
+      pop: true
+    },
+  ],
+  classDiagram: [
+    {
       regex: /\*--|--\*|o--|--o|<--|<-|--/,
       token: "keyword"
     },
+
   ],
   // The multi-line comment state.
   comment: [
